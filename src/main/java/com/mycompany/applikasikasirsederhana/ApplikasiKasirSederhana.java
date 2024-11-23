df4ee2163d77f7571328b1810533fdeb550a2d3c
USER <USER@DESKTOP-9SSI5U6>
2024-11-23 19:16:05 +0700

    stk

--- src/main/java/com/mycompany/applikasikasirsederhana/ApplikasiKasirSederhana.java
+++ src/main/java/com/mycompany/applikasikasirsederhana/ApplikasiKasirSederhana.java
@@ -6,6 +6,6 @@ package com.mycompany.applikasikasirsederhana;
 public class ApplikasiKasirSederhana {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Bitch!!");
     }
 }
